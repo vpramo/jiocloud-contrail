96408264ff0e23cffd41c1c328ee4143e35ac6dc
vpramo <pramod.venkatesh@ril.com>
2015-10-21 13:54:14 +0530

    Disable contrail-dns for Contrail-upgrade

--- manifests/control.pp
+++ manifests/control.pp
@@ -9,30 +9,55 @@ class contrail::control (
   $log_level       = 'SYS_INFO',
   $log_file_size   = 10737418240,
   $log_local       = 1,
+  $enable_dns      = false,
+  $dns_port        = '10000'
 ) {
 
   package {'contrail-control':
     ensure => $package_ensure,
   }
 
-
-  package {'contrail-dns':
-    ensure => $package_ensure,
-  }
+  if $enable_dns {
+    package {'contrail-dns':
+      ensure => $package_ensure,
+    }
   ##
   # DNS configuration
   ##
 
-  file { '/etc/contrail/dns.conf' :
-    ensure  => present,
-    content => template("${module_name}/dns.conf.erb"),
-  }
+    file { '/etc/contrail/dns.conf' :
+      ensure  => present,
+      content => template("${module_name}/dns.conf.erb"),
+    }
 
-  service {'contrail-dns':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/contrail/dns.conf'],
-    require   => Package['contrail-dns']
+  ##
+  # Contrail Named Configuration
+  ##
+
+    file { '/etc/contrail/supervisord_control_files/contrail-named.ini' :
+      ensure => present,
+      source => "puppet:///modules/${module_name}/contrail-named.ini"
+    }
+
+    file { '/etc/contrail/dns/named.conf' :
+      ensure  => present,
+      content => template("${module_name}/contrail-named.conf.erb"),
+    }
+
+    service {'contrail-named':
+      ensure    => running,
+      enable    => true,
+      subscribe => [ File['/etc/contrail/dns/named.conf'],
+                File['/etc/contrail/supervisord_control_files/contrail-named.ini'] ],
+      require   => Package['contrail-dns']
+    }
+
+    service {'contrail-dns':
+      ensure    => running,
+      enable    => true,
+      subscribe => File['/etc/contrail/dns.conf'],
+      require   => Package['contrail-dns']
+    }
   }
 
   ##
